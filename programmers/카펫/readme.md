ce660386c1e29aabd882c9eabbdf890a120b8ccf
hyemin <hmjang28@gmail.com>
2021-09-08 20:45:17 +0900

    test

---
+++ programmers/카펫/readme.md
@@ -0,0 +1,5 @@
+## [프로그래머스 LEVEL2 카펫](https://programmers.co.kr/learn/courses/30/lessons/42842)
+
+카펫의 넓이 = brown + yellow = width x height
+외부 사각형 갯수(brown) = 2w + 2h + 4
+내부 사각형 갯수(yellow) = (x-2) * (y-2)
---
+++ programmers/프린터/장혜민.py
@@ -0,0 +1,29 @@
+# 1차 풀이
+
+priorities = [2,1,3,2]
+location = 2
+
+def solution(priorities, location):
+    cnt = 0
+    arr = []
+    for index, item in enumerate(priorities) :
+        arr.append((item, index))
+    print(arr)   
+    while priorities:          
+        target = arr.pop(0)
+        t = priorities.pop(0)        
+        if len(arr) != 0:
+            if target[0] >= max(priorities) :  # 더 큰 값이 하나도 없으면
+                cnt += 1  # 인쇄    
+                if target[1] == location:
+                    return cnt  
+            else:
+                arr.append(target)  
+                priorities.append(t)
+        else:
+            cnt += 1
+            if target[1] == location:
+                return cnt
+
+
+# print(solution(priorities, location)) 
