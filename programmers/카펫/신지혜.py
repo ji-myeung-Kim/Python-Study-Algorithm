ed6f2e7e4126db38270c617a738b690e2afc9e92
jhshin29 <shinji03264@gmail.com>
2021-09-24 02:26:31 +0900

    programmers carpet

--- programmers/카펫/신지혜.py
+++ programmers/카펫/신지혜.py
@@ -1,3 +1,13 @@
+
 def solution(brown, yellow):
-    answer = []
-    return answer
+    all = brown + yellow
+
+    for h in range(1, all+1):
+        if all % h == 0:
+            w = all/h
+            if w >= h:
+                # print(w,h)
+                if (w-2)*(h-2) == yellow :
+                    return [int(w),h]
+
+print(solution(10, 2))
