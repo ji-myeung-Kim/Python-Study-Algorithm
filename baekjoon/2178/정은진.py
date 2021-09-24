46b6b75d0a0e57c5bf278d1d1965bfa1de6eb26f
zyan <bingbong-party>
2021-09-24 21:57:36 +0900

    [정은진] 2178
    =를 ==라고 써놨네~ 등신;-;

--- baekjoon/2178/정은진.py
+++ baekjoon/2178/정은진.py
@@ -13,22 +13,22 @@ def bfs(x, y) :
     dy = [0, 0, -1, 1]
 
     queue = deque()
-    queue.append((x,y))
+    queue.append((x, y))
 
     while queue :
         x, y = queue.popleft()
 
-        for i in range(4) :
+        for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= N or ny < 0 or ny >= M :
+            if nx < 0 or nx >= N or ny < 0 or ny >= M:
                 continue
-            if graph[nx][ny] == 0 :
+            if graph[nx][ny] == 0:
                 continue 
-            if graph[nx][ny] == 1 :
-                queue.append((nx,ny))
-                graph[nx][ny] == graph[x][y] + 1
+            if graph[nx][ny] == 1:
+                queue.append((nx, ny))
+                graph[nx][ny] = graph[x][y] + 1
 
     return graph[N-1][M-1]
 
