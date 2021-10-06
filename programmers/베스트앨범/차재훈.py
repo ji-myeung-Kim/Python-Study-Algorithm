f7658f7de6d6caa49024dffdae4b5233ecf29135
jh <sjj0077@naver.com>
2021-10-06 20:23:57 +0900

    [차재훈] main readme Fix

--- programmers/베스트앨범/차재훈.py
+++ programmers/베스트앨범/차재훈.py
@@ -44,8 +44,7 @@ def solution(genres, plays):
     
     # 장르별 총 재생횟수가 저장된 딕셔너리를 내림차순으로 정렬해준다.
     sorted_total_list = sorted(total_dict.items(), key=lambda x:x[1], reverse=True)
-    print(f'{sorted(total_dict, key=lambda x:total_dict[x], reverse=True)} -------------------------------value')
-    print(f'{sorted_total_list} -------------------------------sorted')
+    
     # 내림차순으로 정렬된 장르별 총 재생횟수 딕셔너리를 반복한다.
     for key in sorted_total_list:
         # 장르별 노래들의 재생횟수가 저장된 딕셔너리의 장르별 재생횟수(value)를 내림차순으로 정렬한다.
@@ -67,9 +66,10 @@ sorted(total_dict, key=lambda x:total_dict[x], reverse=True)으로 정렬을 했
 key[0]과 같은 복잡한 형태가 아닌 key로 검색이 가능해서 조금더 직관적?인 코드가 되지 않을까 싶다.
 """
 if __name__ == '__main__':
-    genres = ["classic", "pop", "classic", "classic", "pop"]
-    genres2 = ["classic", "pop", "classic", "classic","jazz","pop", "Rock", "jazz"]
-    plays = [500, 600, 150, 800, 2500]
-    plays2 = [500, 600, 150, 800, 1100, 2500, 100, 1000]
-    print(solution(genres, plays)) # [4, 1, 3, 0]
-    print(solution(genres2, plays2)) # [5, 1, 4, 7, 3, 0, 6]
+    # genres = ["classic", "pop", "classic", "classic", "pop"]
+    # genres2 = ["classic", "pop", "classic", "classic","jazz","pop", "Rock", "jazz"]
+    # plays = [500, 600, 150, 800, 2500]
+    # plays2 = [500, 600, 150, 800, 1100, 2500, 100, 1000]
+    # print(solution(genres, plays)) # [4, 1, 3, 0]
+    # print(solution(genres2, plays2)) # [5, 1, 4, 7, 3, 0, 6]
+    
