0f4901556a6f81ff6a80b3044ad97ab468a81bd3
kingdomunder <king4525@gmail.com>
2021-10-28 10:18:00 +0900

    [임우송]베스트앨범 텍스트주석 수정

--- programmers/베스트앨범/임우송.py
+++ programmers/베스트앨범/임우송.py
@@ -1,23 +1,4 @@
 # 2. 베스트앨범 / 해시
-# https://programmers.co.kr/learn/courses/30/lessons/42579
-# ---------문제 설명----------------------------------------------------------------------
-# 스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 [두 개씩] 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
-# 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
-# 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
-# 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
-# 노래의 [장르]를 나타내는 문자열 배열 genres와 [노래별 재생 횟수]를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
-
-# 제한사항
-# genres[i]는 [고유번호]가 i인 노래의 장르입니다.
-# plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
-# genres와 plays의 길이는 같으며, 이는 1 이상 10,000 이하입니다.
-# 장르 종류는 100개 미만입니다.
-# 장르에 속한 곡이 하나라면, 하나의 곡만 선택합니다.
-# 모든 장르는 재생된 횟수가 다릅니다.
-
-# 입출력 예
-# genres	                                        plays                   	return
-# ["classic", "pop", "classic", "classic", "pop"]	[500, 600, 150, 800, 2500]	[4, 1, 3, 0]
 
 # ---------문제 해석---------------------------------------------------------------------- 
 # 장르마다 최대 두 개까지의 노래를 수록한다.(재생횟수가 많은 두 곡)
@@ -46,14 +27,9 @@ def solution(genres, plays):
         sumGenres.setdefault(genres[i], 0)   # {장르 : 장르별 재생횟수의 합} 해시 생성  -> 재생횟수 많은 장르별로 가장 먼저 정렬하기 위한 dictionary
         sumGenres[genres[i]] += plays[i]    # 장르(key)가 같으면 재생횟수(value)를 중복해서 더해준다 
 
-    print("hash : ", hash)
-    print("sum : ", sumGenres)
     # 재생횟수를 기준으로 내림차순 정렬 
     hash = sorted(hash.items(), reverse=True, key = lambda item : item[1][1])   
     sumGenres = sorted(sumGenres.items(), reverse=True, key = lambda item : item[1])
-
-    print("hash : ", hash)
-    print("sum : ", sumGenres)
 
     index = 0
     while index < len(sumGenres):   # 정렬된 장르를 기준으로, 모든 장르 검색이 끝날때까지 반복 
@@ -65,16 +41,9 @@ def solution(genres, plays):
                     count += 1  # 한 장르의 노래를 추가할 떄마다 누적
         index += 1
 
-    print(answer)
     return answer
 
 solution(genres, plays)
-
-# return = [4, 1, 3, 0]
-
-# ----------------------------------------------------------------------------------------
-
-# 프로그래머스 코드정확도 채점 - 합계:  / 100.0
 
 
 
