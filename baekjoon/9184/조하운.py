418004204f33e1bd0e013b0d14f5a623aba767c7
noowah <henrynoowah@gmail.com>
2021-11-03 02:58:40 +0900

    [SOLVED]조하운

--- baekjoon/9184/조하운.py
+++ baekjoon/9184/조하운.py
@@ -20,6 +20,8 @@ boolean = True
 
 # 메모이제이션 DP
 
+dict = {}
+
 def w(a, b, c):
     if a <= 0 or b <= 0 or c <= 0:
         return 1
@@ -48,3 +50,33 @@ while True:
     if a == -1 and b == -1 and c == -1:
         break
     print(f"w({a}, {b}, {c}) = {w(a,b,c)}")
+
+import sys
+
+#  # 문제 조건에서 a, b, c가 20보다 크면 모두 w(20, 20, 20)으로 통일시키기 때문에 3차원배열의 각 원소는 0~20까지만 필요함
+# memo = dict()
+
+# def w(a, b, c):    
+#     if a<=0 or b<=0 or c<=0 :
+#         return 1
+#     if a>20 or b>20 or c>20 :
+#         return w(20, 20, 20)
+
+#     # 메모이제이션 해둔 값이 있다면 바로 반환한다
+#     if (a,b,c) in memo:
+#         return memo[(a,b,c)]
+#     else:           
+#         if a < b < c:
+#             memo[(a,b,c)] = w(a, b, c-1) + w(a, b-1, c-1) - w(a, b-1, c)
+#             return memo[(a,b,c)]
+#         else:
+#             memo[(a,b,c)] =  w(a-1, b, c) + w(a-1, b-1, c) + w(a-1, b, c-1) - w(a-1, b-1, c-1)
+#             return memo[(a,b,c)]
+
+# while True:    
+#     a, b, c = list(map(int, sys.stdin.readline().split(" ")))
+
+#     if a == b == c == -1:
+#         break
+
+#     print("w(%d, %d, %d) = %d"%(a, b, c, w(a,b,c)))
