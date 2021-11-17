3dd152230cb8d6db90da964d09078dd928eaab4e
bingbong-party <ww9720@gmail.com>
2021-11-18 00:39:59 +0900

    [정은진] 백준 1012수정

--- baekjoon/1012/정은진.py
+++ baekjoon/1012/정은진.py
@@ -1,58 +1,39 @@
+import sys
+sys.setrecursionlimit(10000)
+
 def ans(lists, x, y):
     global answer
-    # temp = lists.pop()
 
     if (x, y-1) in lists:
         lists.remove((x, y-1))
         ans(lists, x, y-1)
-        # answer += 1
     elif (x, y+1) in lists:
         lists.remove((x, y+1))
         ans(lists, x, y+1)
-        # answer += 1
     elif (x-1, y) in lists:
         lists.remove((x-1, y))
         ans(lists, x-1, y)
-        # answer += 1
     elif (x+1, y) in lists:
         lists.remove((x+1, y))
         ans(lists, x+1, y)
-        # answer += 1
-    # else:
-    #     answer += 1
-    #
-    # if (temp[0], temp[1]-1) in lists:
-    #     ans(lists)
-    #     # answer.append((temp[0], temp[1]-1))
-    #     # lists.remove((temp[0], temp[1]-1))
-    # elif (temp[0], temp[1]+1) in lists:
-    #     ans
-    # else:
-    #     answers.append(temp)
 
-import sys
+
 input = sys.stdin.readline
 
-input()
-input()
+testcase = int(input())
 
-testl = list(tuple(map(int, input().split())))
-print(testl)
+for i in range(testcase):
+    answer = 0
+    graphlist = list(map(int, input().split()))
+    testl = []
 
-# mydata = list(map(int, input().split()))
-# print(mydata)
+    for j in range(graphlist[2]):
+        a, b = list(map(int, input().split()))
+        testl.append((a, b))
 
-#
-#
-# testl = [(0, 0), (1, 0), (1, 1), (4, 2), (4, 3), (4, 5),
-#          (2, 4), (3, 4), (7, 4), (8, 4), (9, 4), (7, 5),
-#          (8, 5), (9, 5), (7, 6), (8, 6), (9, 6)]
-# print(testl[2])
-# answer = 0
-#
-# while len(testl)!=0:
-#     temp = testl.pop()
-#     ans(testl, temp[0], temp[1])
-#     answer += 1
-#
-# print(answer)
+    while len(testl) != 0:
+        temp = testl.pop()
+        ans(testl, temp[0], temp[1])
+        answer += 1
+
+    print(answer)
