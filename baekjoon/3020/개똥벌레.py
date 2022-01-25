9e905fc30b41173fe0098d8651bfcc5509b64544
ji-myeung-Kim <wlaud5314@naver.com>
2022-01-25 09:38:21 +0900

    [백준 3020 풀이]

---
+++ baekjoon/3020/개똥벌레.py
@@ -0,0 +1,47 @@
+
+import sys
+
+def Binary_Search_Upper(data_list, x):                  #주어진 list에서 x보다 큰 데이터의 개수를 반환; log n 안에 찾음
+    left = 0
+    right = len(data_list) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if data_list[mid] <= x:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return len(data_list) - (right + 1)   
+
+n, h = map(int, input().split())
+
+down = []
+up = []
+for i in range(n):
+    input_list = sys.stdin.readline().split()
+    input_num = int(input_list[0])
+
+
+    if i % 2 == 0:
+        down.append(int(input_num()))
+    else:
+        up.append(int(input_num()))
+
+down.sort()
+up.sort()
+
+min_count = n
+range_count = 0
+
+
+for i in range(1, h + 1):
+    down_count = Binary_Search_Upper(down, i- 1)
+    top_count =  Binary_Search_Upper(up, h-i)
+    now_count = down_count + top_count
+
+    if now_count < min_count:
+        min_count = now_count
+        range_count = 1
+    
+    elif now_count == min_count:
+        range_count +=1
+print(min_count, range_count)
